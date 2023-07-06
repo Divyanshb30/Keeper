c554a940f8e1c2958c65b7444b2e5fc55135e86d
div <divyanshb30@gmail.com>
2023-07-07 02:43:18 +0530

    Keeper-Frontend

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NotesIcon from '@mui/icons-material/Notes';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NotesIcon />
+          Keeper
+        </h1>
+    </header>
+  );
+}
+
+export default Header;
